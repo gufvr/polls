a03bd111fdb41fc8de5a2061fb6197426be04204
gustavo <gustavo.vfavero@gmail.com>
2024-02-07 22:04:49 -0300

    feat: Integrate Redis for real-time poll result tracking
    - Implemented Redis client initialization to handle real-time vote tracking.
    - Utilized Redis commands to update and retrieve poll vote counts.
    - Added logic to handle cases where the poll is not found, returning an appropriate error message.
    - Enhanced endpoint response to include poll details along with the current vote counts for each option.

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import z from 'zod'
 import { randomUUID } from 'node:crypto'
 import { prisma } from '../../lib/prisma'
 import { FastifyInstance } from 'fastify'
+import { redis } from '../../lib/redis'
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -29,12 +30,13 @@ export async function voteOnPoll(app: FastifyInstance) {
       })
 
       if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
-
         await prisma.vote.delete({
           where: {
             id: userPreviousVoteOnPoll.id
           }
         })
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({ message: 'You already voted on this poll.' }) 
       }
@@ -59,6 +61,7 @@ export async function voteOnPoll(app: FastifyInstance) {
       }
     })
 
+    await redis.zincrby(pollId, 1, pollOptionId)
       
       return reply.status(201).send()
     })
